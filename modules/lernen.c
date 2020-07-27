22211b9685db5bb331f78fa9d59153477ac7311c
settstep <68824743+settstep@users.noreply.github.com>
2020-07-27 22:34:17 +0200

    file lernen.c added

---
+++ modules/lernen.c
@@ -0,0 +1,12 @@
+/**
+ * \file    lernen.c
+ *
+ * \date    2020-07-27
+ *
+ * \author  settstep
+ *
+ * \section sec_lernen_c_history Historie
+ *
+ * \par     2020-07-27 settstep
+ * - file created
+ */
